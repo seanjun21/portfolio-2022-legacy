a3a52d0aef3f4c7753d73b0d7a10582c07c24592
Sean Jun <seanjun21@gmail.com>
2021-10-08 01:29:28 -0700

    Update to latest Mate Portfolio

---
+++ src/gatsby-theme-mate/icons.js
@@ -0,0 +1,19 @@
+import {
+  faMedium,
+  faTwitter,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faGlobe,
+  faEnvelope,
+  faFilePdf,
+} from '@fortawesome/free-solid-svg-icons';
+
+export const ICONS = [
+  faMedium,
+  faTwitter,
+  faGithub,
+  faGlobe,
+  faEnvelope,
+  faFilePdf
+];
